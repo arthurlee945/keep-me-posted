7d9bba928c66e321190024b407c228366df833e1
Arthur Lee <arthur.lee945@gmail.com>
2023-07-31 20:10:15 -0500

    ud

---
+++ src/app/Header.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+interface HeaderProps {}
+
+const Header: FC<HeaderProps> = () => {
+    return <header className="w-full p-4 flex items-center justify-between border-b-2 border-neutral-100">HI</header>;
+};
+
+export default Header;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import "@/styles/font-face.css";
 import type { Metadata } from "next";
+import Header from "./Header";
 
 export const metadata: Metadata = {
     title: "Keep Me Posted",
@@ -10,7 +11,10 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
-            <body>{children}</body>
+            <body className="bg-zinc-950 m-4 border-2 border-neutral-100 min-h-[calc(100vh-32px)] text-white">
+                <Header />
+                {children}
+            </body>
         </html>
     );
 }
