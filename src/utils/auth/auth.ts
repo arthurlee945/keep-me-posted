f1151a06ae33211f46119e76bb6b77624d429562
Arthur Lee <arthur.lee945@gmail.com>
2023-08-15 19:17:41 -0500

    update

--- src/utils/auth/auth.ts
+++ src/utils/auth/auth.ts
@@ -3,96 +3,89 @@ import { compare } from "bcryptjs";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
 import { prisma } from "../database/prisma";
-// import { createJwtToken, fromDate, generateRandomToken, signOutUser, userIsValid } from "../functions/authUtils";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-// import { redirect } from "next/navigation";
-// import { cookies } from "next/headers";
+
 const adapter = PrismaAdapter(prisma);
 export const authOptions: NextAuthOptions = {
-  session: {
-    strategy: "jwt",
-    maxAge: 15 * 24 * 60 * 60,
-    updateAge: 24 * 60 * 60,
-  },
-  adapter,
-  providers: [
-    CredentialsProvider({
-      name: "KMP-credentials",
-      credentials: {
-        email: { label: "Email", type: "email" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials) {
-        if (!credentials?.email || !credentials?.password)
-          throw new Error("Email or Password Isn't Provided");
-        try {
-          const user = await prisma.user.findUnique({
-            where: {
-              email: credentials.email,
+    session: {
+        strategy: "jwt",
+        maxAge: 15 * 24 * 60 * 60,
+        updateAge: 24 * 60 * 60,
+    },
+    adapter,
+    providers: [
+        CredentialsProvider({
+            name: "KMP-credentials",
+            credentials: {
+                email: { label: "Email", type: "email" },
+                password: { label: "Password", type: "password" },
             },
-          });
-          if (
-            !user ||
-            !user.password ||
-            !(await compare(credentials.password, user.password))
-          )
-            throw new Error("Email or Password Is Wrong", {
-              cause: "INCORRECT_INPUT",
-            });
-          const { id, name, email } = user;
-          return {
-            id,
-            name,
-            email,
-            test: "test",
-          };
-        } catch (err) {
-          if (err instanceof Error && err.message) {
-            throw new Error(err.message, { cause: err });
-          }
-          throw new Error("Something Went Wrong In The Server", { cause: err });
-        }
-      },
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID || "test",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "test",
-    }),
-  ],
-  pages: {
-    signIn: "/auth/signin",
-    signOut: "/auth/signout",
-  },
-  callbacks: {
-    //* for Database strategy
-    // async signIn({ user, account, profile, email, credentials }) {
-    //     if (account?.provider !== "credentials" || !user || !adapter.createSession) return true;
-    //     const sessionToken = createJwtToken(user.id);
-    //     const sessionExp = fromDate(15 * 24 * 60 * 60);
-    //     await adapter.createSession({
-    //         sessionToken,
-    //         userId: user.id,
-    //         expires: sessionExp,
-    //     });
-    //     cookies().set("next-auth.session-token", sessionToken, {
-    //         expires: sessionExp,
-    //     });
-    //     return true;
-    // },
-    session: async ({ session, token, user }) => {
-      return {
-        ...session,
-        user: {
-          ...session.user,
-          iat: token.iat,
+            async authorize(credentials) {
+                if (!credentials?.email || !credentials?.password) throw new Error("Email or Password Isn't Provided");
+                try {
+                    const user = await prisma.user.findUnique({
+                        where: {
+                            email: credentials.email,
+                        },
+                    });
+                    if (!user || !user.password || !(await compare(credentials.password, user.password)))
+                        throw new Error("Email or Password Is Wrong", {
+                            cause: "INCORRECT_INPUT",
+                        });
+                    const { id, name, email } = user;
+                    return {
+                        id,
+                        name,
+                        email,
+                        test: "test",
+                    };
+                } catch (err) {
+                    if (err instanceof Error && err.message) {
+                        throw new Error(err.message, { cause: err });
+                    }
+                    throw new Error("Something Went Wrong In The Server", { cause: err });
+                }
+            },
+        }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID || "test",
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET || "test",
+        }),
+    ],
+    pages: {
+        signIn: "/auth/signin",
+        signOut: "/auth/signout",
+    },
+    callbacks: {
+        //* for Database strategy
+        // async signIn({ user, account, profile, email, credentials }) {
+        //     if (account?.provider !== "credentials" || !user || !adapter.createSession) return true;
+        //     const sessionToken = createJwtToken(user.id);
+        //     const sessionExp = fromDate(15 * 24 * 60 * 60);
+        //     await adapter.createSession({
+        //         sessionToken,
+        //         userId: user.id,
+        //         expires: sessionExp,
+        //     });
+        //     cookies().set("next-auth.session-token", sessionToken, {
+        //         expires: sessionExp,
+        //     });
+        //     return true;
+        // },
+        session: async ({ session, token, user }) => {
+            return {
+                ...session,
+                user: {
+                    ...session.user,
+                    iat: token.iat,
+                },
+            };
         },
-      };
+        jwt: async ({ token, account, profile }) => {
+            return {
+                ...token,
+            };
+        },
     },
-    jwt: async ({ token, account, profile }) => {
-      return {
-        ...token,
-      };
-    },
-  },
-  events: {},
+    events: {},
 };
--- src/utils/functions/authUtils.ts
+++ src/utils/functions/authUtils.ts
@@ -1,10 +1,5 @@
 import { randomBytes, createHash } from "crypto";
-import { prisma } from "../database/prisma";
-import { getServerSession } from "next-auth/next";
-import { authOptions } from "../auth/auth";
-import { redirect } from "next/navigation";
-import type { Session } from "next-auth/src/core/types";
-// import jwt from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 
 export const generateRandomToken = () => {
     const resetToken = randomBytes(32).toString("hex");
@@ -20,41 +15,41 @@ export const hashToken = (token: string) => {
     return createHash("sha256").update(token).digest("hex");
 };
 
-// export const createJwtToken = (id: string) => {
-//     if (!process.env.NEXTAUTH_SECRET) throw new Error("JWT SECRET required");
-//     return jwt.sign({ id }, process.env.NEXTAUTH_SECRET);
-// };
+export const createJwtToken = (id: string) => {
+    if (!process.env.NEXTAUTH_SECRET) throw new Error("JWT SECRET required");
+    return jwt.sign({ id }, process.env.NEXTAUTH_SECRET);
+};
 export const fromDate = (time: number, date = Date.now()) => {
     return new Date(date + time * 1000);
 };
-export const signOutUser = async () => {
-    try {
-        const { csrfToken } = await (await fetch(`${process.env.APP_URL}/api/auth/csrf`)).json();
-        const fetchOptions = {
-            method: "post",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                csrfToken,
-                callbackUrl: process.env.APP_URL,
-                json: true,
-            }),
-        };
-        const res = await fetch(`${process.env.APP_URL}/api/auth/signout`, fetchOptions);
-        return res.status === 200;
-    } catch (err) {
-        return false;
-    }
-};
+// export const signOutUser = async () => {
+//     try {
+//         const { csrfToken } = await (await fetch(`${process.env.APP_URL}/api/auth/csrf`)).json();
+//         const fetchOptions = {
+//             method: "post",
+//             headers: {
+//                 "Content-Type": "application/json",
+//             },
+//             body: JSON.stringify({
+//                 csrfToken,
+//                 callbackUrl: process.env.APP_URL,
+//                 json: true,
+//             }),
+//         };
+//         const res = await fetch(`${process.env.APP_URL}/api/auth/signout`, fetchOptions);
+//         return res.status === 200;
+//     } catch (err) {
+//         return false;
+//     }
+// };
 
-export const userIsValid = async () => {
-    const session = (await getServerSession(authOptions)) as Session & { user: { iat: number } };
-    if (!session || !session.user || !session.user.email || !session.user.iat) return;
+// export const userIsValid = async () => {
+//     const session = (await getServerSession(authOptions)) as Session & { user: { iat: number } };
+//     if (!session || !session.user || !session.user.email || !session.user.iat) return;
 
-    const user = await prisma.user.findFirst({
-        where: { email: session.user?.email },
-        select: { passwordChangedAt: true },
-    });
-    if (!user || (user.passwordChangedAt && user.passwordChangedAt.getTime() > session.user.iat * 1000)) redirect("/auth/signout");
-};
+//     const user = await prisma.user.findFirst({
+//         where: { email: session.user?.email },
+//         select: { passwordChangedAt: true },
+//     });
+//     if (!user || (user.passwordChangedAt && user.passwordChangedAt.getTime() > session.user.iat * 1000)) redirect("/auth/signout");
+// };
